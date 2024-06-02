f1e2f1ed795dcc3fc89901a9251d1a8cc89d74df
Adil Isani <adil.isani1@gmail.com>
2024-06-02 18:14:31 +0500

    backend updated

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const dishRouter = require('./routes/dishes');
-const userRouter = require('./routes/userRoute');
 
 const port = process.env.PORT || 5000;
 const app = express();
@@ -11,11 +10,11 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 app.use("/api", dishRouter)
-app.use("/auth", userRouter)
+
 
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log("DBConnection Successfull"))
-    .catch((err) => console.log(err))
+    .catch((err) => console.log("DB Connection Failed: ", err.message))
 
 app.listen(port, () => {
     console.log(`Server is running on port http://localhost:${port}`)
